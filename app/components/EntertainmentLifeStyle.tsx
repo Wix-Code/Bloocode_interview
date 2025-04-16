b427d0ab54584f3ff366ca91791b560ceecdc876
Wix-Code <ogbonna428alex@gmail.com>
2025-04-16 18:19:01 +0100

    push

--- app/components/EntertainmentLifeStyle.tsx
+++ app/components/EntertainmentLifeStyle.tsx
@@ -23,18 +23,18 @@ const EntertainmentLifeStyle = () => {
     slidesToScroll: 5
   };
   return (
-    <div className='mt-10'>
-      <div className='max-w-[1200px] mx-auto'>
-        <div className='flex items-center justify-between mb-5 mx-10'>
-          <h1 className='text-[#5A5A5A] font-[700] max-sm:text-[16px] text-[20px] border-l-[3px] border-[#CC0001] pl-1'>Entertainment & Lifestyle</h1>
-          <button className='px-4 py-2 max-sm:text-[13px] rounded-[22px] text-[#9747FF] text-[15px] font-[500] border-[#9747FF] cursor-pointer border-[1px] flex justify-center items-center gap-1'>View all <IoIosArrowForward /></button>
+     <div className='mt-10'>
+      <div className='max-w-[1200px] max-sm:mx-5 mx-auto'>
+        <div className='flex items-center max-sm:text-[16px] justify-between mb-5 max-sm:mx-0 mx-10'>
+          <h1 className='text-[#5A5A5A] font-[700] max-sm:text-[14px] text-[20px] border-l-[3px] border-[#CC0001] pl-1'>Entertainment & Lifestyle</h1>
+          <button className='px-4 py-2 max-sm:text-[11px] max-sm:p-2 rounded-[30px] text-[#9747FF] text-[15px] font-[500] border-[#9747FF] cursor-pointer border-[1px] flex justify-center items-center gap-1'>View all <IoIosArrowForward /></button>
         </div>
         <div className="flex items-start gap-5 overflow-x-auto scroll-smooth snap-x snap-mandatory px-4 pb-4 hide-scrollbar ">
           {
             categories.map((category: Category) => (
-              <div key={category.id} className="snap-start bg-[#F4F4F4] p-3 cursor-pointer shrink-0 w-[223px] flex flex-col gap-3">
-                <img className='w-[210px] h-[210px] object-cover' src={category.img} alt="" />
-                <h1 className='text-[#282828] font-[700] text-[18px]'>{category.name}</h1>
+              <div key={category.id} className="snap-start bg-[#F4F4F4] max-sm:w-[180px] p-3 cursor-pointer shrink-0 w-[223px] flex flex-col gap-3">
+                <img className='w-[100%] h-[210px] max-sm:h-[150px] object-cover' src={category.img} alt="" />
+                <h1 className='text-[#282828] max-sm:text-[16px] font-[700] text-[18px]'>{category.name}</h1>
                 <div className='flex items-center gap-2'>
                   <button className='bg-[#d6d6d6] w-[30px] h-[30px] rounded-[50%] flex justify-center items-center'><img src="/files/share.png" /></button>
                   <button className='bg-[#d6d6d6] w-[30px] h-[30px] rounded-[50%] flex justify-center items-center'><img src="/files/gr.png" /></button>
--- app/components/NewlyAddedEpisodes.tsx
+++ app/components/NewlyAddedEpisodes.tsx
@@ -4,16 +4,16 @@ import { podcasts } from '../dummyData'
 const NewlyAddedEpisodes = () => {
   return (
     <div>
-      <div className='max-w-[1200px] mx-auto overflow-hidden'>
+      <div className='max-w-[1200px] max-sm:mx-5 mx-auto overflow-hidden'>
         <p className='text-[24px] text-[#282828] font-[700]'>Newly added episodes</p>
         <div className="flex items-start gap-5 overflow-x-auto scroll-smooth snap-x snap-mandatory px-4 pb-4 hide-scrollbar">
           {
             podcasts.map((podcast) => {
               return (
-                <div key={podcast.id} className="snap-start cursor-pointer shrink-0 w-[223px] flex flex-col gap-3">
-                  <img className='w-[100%] h-[187px]' src={podcast.img} alt="" />
+                <div key={podcast.id} className="snap-start max-sm:w-[180px] max-sm:gap-2 cursor-pointer shrink-0 w-[223px] flex flex-col gap-3">
+                  <img className='w-[100%] object-cover max-sm:h-[150px] h-[187px]' src={podcast.img} alt="" />
                   <p className='text-[13px] font-[700] text-[#828282]'>AUG 29, 2023</p>
-                  <p className='text-[#282828] font-[700] text-[16px]'>{podcast.desc}</p>
+                  <p className='text-[#282828] font-[700] max-sm:text-[14px] text-[16px]'>{podcast.desc}</p>
                   <div className='flex items-center gap-7'>
                     <p className='text-[13px] font-[500] text-[#828282]'>More Episodes</p>
                     <div className='flex items-center gap-2'>
