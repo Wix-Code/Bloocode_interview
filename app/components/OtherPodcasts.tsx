e0bd46ef18c8462ee2ec0d1a7701d8c3d417408a
Wix-Code <ogbonna428alex@gmail.com>
2025-04-18 01:31:09 +0100

    push

--- app/components/OtherPodcasts.tsx
+++ app/components/OtherPodcasts.tsx
@@ -54,16 +54,16 @@ const OtherPodcasts = () => {
 
   return (
     <div className='mt-10'>
-      <div className='max-w-[1200px] mx-auto'>
-        <div className='flex items-center  max-sm:text-[16px] justify-between mb-5 max-sm:mx-0 mx-10'>
-          <h1 className='text-[#5A5A5A] max-xl:mx-5 font-[700] max-sm:text-[14px] text-[20px] border-l-[3px] border-[#CC0001] pl-1'>Other Podcasts</h1>
-          <Link href="/categories"><button className='px-4 py-2 max-sm:text-[11px] max-sm:p-2 rounded-[30px] text-[#9747FF] text-[15px] max-xl:mx-5 font-[500] border-[#9747FF] cursor-pointer border-[1px] flex justify-center items-center gap-1'>View all <IoIosArrowForward /></button></Link>
+      <div className='max-w-[1200px] overflow-hidden max-xl:mx-2 mx-auto'>
+        <div className='flex items-center  max-xl:mx-3  max-sm:text-[16px] justify-between mb-5 max-sm:mx-0 mx-10'>
+          <h1 className='text-[#5A5A5A] font-[700] max-sm:text-[14px] text-[20px] border-l-[3px] border-[#CC0001] pl-1'>Other Podcasts</h1>
+          <Link href="/categories"><button className='px-4  max-xl:mx-3 py-2 max-sm:text-[11px] max-sm:p-2 rounded-[30px] text-[#9747FF] text-[15px] font-[500] border-[#9747FF] cursor-pointer border-[1px] flex justify-center items-center gap-1'>View all <IoIosArrowForward /></button></Link>
         </div>
-        <div className="custom-slider-wrapper items-start max-xl:px-3 gap-5 pb-4">
+        <div className="custom-slider-wrapper max-xl:w-full items-start gap-5 pb-4">
           <Slider {...settings}>
           {
             podcasts?.data?.map((category) => (
-              <div key={category.id} className="snap-start bg-[#F4F4F4] max-sm:w-[180px] p-3 cursor-pointer shrink-0 w-[223px] flex flex-col gap-3">
+              <div key={category.id} className="snap-start bg-[#F4F4F4] max-sm:w-[180px] p-3 cursor-pointer w-[223px] flex flex-col gap-3">
                 <img className='w-[100%] h-[210px] max-sm:h-[150px] object-cover' src={category.picture_url} alt="" />
                 <h1 className='text-[#282828] max-sm:text-[16px] font-[700] my-3 text-[18px]'>{category.title.slice(0, 15)}...</h1>
                 <div className='flex items-center gap-2'>
--- app/components/TrendingThisWeek.tsx
+++ app/components/TrendingThisWeek.tsx
@@ -1,29 +1,64 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { podcasts } from '../dummyData'
 import { usePodcasts } from '../utils/podcastQuerry';
+import { RiPlayLargeFill, RiPlayReverseLargeFill } from 'react-icons/ri';
 
 const TrendingThisWeek = () => {
   const [page, setPage] = useState(1);
   const { data: podcasts, isLoading, error, isFetching } = usePodcasts(page);
+  const scrollRef = useRef<HTMLDivElement>(null)
+  
+    const scrollLeft = () => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollBy({ left: -300, behavior: 'smooth' })
+      }
+    }
+  
+    const scrollRight = () => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollBy({ left: 300, behavior: 'smooth' })
+      }
+    }
+  
   return (
     <div className=''>
       <div className='max-w-[1200px] max-xl:mx-5 mx-auto mt-5'>
         <p className='text-[24px] max-sm:text-[15px] mb-3 mt-5 text-[#282828] font-[700]'>Trending this week</p>
         <h1 className='text-[#5A5A5A] font-[700] max-sm:text-[14px] text-[20px] border-l-[3px] border-[#CC0001] pl-1'>Educational</h1>
-        <div  className="flex items-start mt-5 gap-5 overflow-x-auto scroll-smooth snap-x snap-mandatory px-4 pb-4 hide-scrollbar">
-          {
-            podcasts?.data?.map((podcast) => { 
-              return (
-                <div key={podcast.id} className="snap-start relative max-sm:w-[180px] max-sm:gap-1 cursor-pointer shrink-0 w-[280px] flex flex-col gap-3">
-                  <img className='w-[100%] object-cover max-sm:h-[300px] h-[400px]' src={podcast.picture_url} alt="" />
-                  <div className='flex flex-col gap-1 absolute px-4 py-3 bottom-0 left-0 w-[100%] bg-[#000000d7]'>
-                    <p className='text-[#FFFFFF] max-sm:text-[11px] text-[13px] font-[700]'>12 Episodes</p>
-                    <p className='text-[20px] max-sm:text-[14px] font-[700] text-[#ffffff]'>{podcast.title.slice(0, 20)}...</p>
+        <div className='relative max-xl:w-full'>
+
+          <div className='absolute  z-10 max-xl:left-0 right-0 top-0 bottom-0 m-auto left-0 flex justify-end items-center'>
+            <div className='bg-[#F9F9F9] p-2 shadow-sm rounded-[12px] gap-2 flex items-center justify-center'>
+              <button
+                onClick={scrollLeft}
+                className="text-[22px] cursor-pointer text-[#5A5A5A]"
+              >
+                <RiPlayReverseLargeFill />
+              </button>
+
+              <button
+                onClick={scrollRight}
+                className="text-[22px] cursor-pointer text-[#5A5A5A]"
+              >
+                <RiPlayLargeFill />
+              </button>
+            </div>
+          </div>
+          <div ref={scrollRef} className="flex items-start mt-5 gap-5 overflow-x-auto scroll-smooth snap-x snap-mandatory px-4 pb-4 hide-scrollbar">
+            {
+              podcasts?.data?.map((podcast) => { 
+                return (
+                  <div key={podcast.id} className="snap-start relative max-sm:w-[180px] max-sm:gap-1 cursor-pointer shrink-0 w-[280px] flex flex-col gap-3">
+                    <img className='w-[100%] object-cover max-sm:h-[300px] h-[400px]' src={podcast.picture_url} alt="" />
+                    <div className='flex flex-col gap-1 absolute px-4 py-3 bottom-0 left-0 w-[100%] bg-[#000000d7]'>
+                      <p className='text-[#FFFFFF] max-sm:text-[11px] text-[13px] font-[700]'>12 Episodes</p>
+                      <p className='text-[20px] max-sm:text-[14px] font-[700] text-[#ffffff]'>{podcast.title.slice(0, 20)}...</p>
+                    </div>
                   </div>
-                </div>
-              )
-             })
-          }
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     </div>
